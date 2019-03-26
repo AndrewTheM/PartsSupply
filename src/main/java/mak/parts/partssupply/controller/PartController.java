066236d08df4f7e75094ec89836e1729f3b82ad3
Andrew <1Hlamidomonad>
2019-03-26 17:24:50 +0200

    Changes to work with Angular

--- src/main/java/mak/parts/partssupply/controller/PartController.java
+++ src/main/java/mak/parts/partssupply/controller/PartController.java
@@ -1,80 +1,42 @@
 package mak.parts.partssupply.controller;
 
-import mak.parts.partssupply.form.PartForm;
 import mak.parts.partssupply.model.Part;
 import mak.parts.partssupply.service.part.impls.PartServiceMongoImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
+@CrossOrigin("*")
 @RequestMapping("/api/part")
 public class PartController {
 
     @Autowired
     private PartServiceMongoImpl partService;
 
-    @RequestMapping(value = "/list", method = RequestMethod.GET)
-    public String partList(Model model) {
-        model.addAttribute("parts", partService.getAll());
-        return "partList";
+    @RequestMapping("/list")
+    public List<Part> getAllParts() {
+        return partService.getAll();
     }
 
-    @RequestMapping(value = "/create", method = RequestMethod.GET)
-    public String createPart(Model model){
-        PartForm partForm = new PartForm();
-        model.addAttribute("partForm", partForm);
-        return "createPart";
+    @RequestMapping("/create/{code}/{name}/{type}/{price}/{annotation}")
+    public Part createPart(@PathVariable("code") String code, @PathVariable("name") String name,
+                       @PathVariable("type") String type, @PathVariable("price") double price,
+                       @PathVariable("annotation") String annotation){
+        return partService.create(new Part(code, name, type, price, annotation));
     }
 
-    @RequestMapping(value = "/create", method = RequestMethod.POST)
-    public String createPart(Model model, @ModelAttribute("partForm") PartForm partForm) {
-        if (!partForm.getCode().isEmpty() && !partForm.getName().isEmpty()
-                && !partForm.getType().isEmpty() && partForm.getPrice() > 0)
-            partService.create(new Part(partForm.getCode(), partForm.getName(), partForm.getType(),
-                                        partForm.getPrice(), partForm.getAnnotation()));
-        model.addAttribute("parts", partService.getAll());
-        return "partList";
+    @RequestMapping("/edit/{id}/{code}/{name}/{type}/{price}/{annotation}")
+    public Part editPart(@PathVariable("id") String id, @PathVariable("code") String code,
+                           @PathVariable("name") String name, @PathVariable("type") String type,
+                           @PathVariable("price") double price, @PathVariable("annotation") String annotation) {
+        return partService.update(new Part(id, code, name, type, price, annotation));
     }
 
-    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
-    public String editPart(Model model, @PathVariable("id") String id) {
-        Part part = partService.get(id);
-        PartForm partForm = new PartForm();
-        partForm.setCode(part.getCode());
-        partForm.setName(part.getName());
-        partForm.setType(part.getType());
-        partForm.setPrice(part.getPrice());
-        partForm.setAnnotation(part.getAnnotation());
-        model.addAttribute("partForm", partForm);
-        return "editPart";
-    }
-
-    @RequestMapping(value = "/edit/{id}", method = RequestMethod.POST)
-    public String editPart(Model model, @PathVariable("id") String id,
-                           @ModelAttribute("partForm") PartForm partForm) {
-        if (!partForm.getCode().isEmpty() && !partForm.getName().isEmpty()
-                && !partForm.getType().isEmpty() && partForm.getPrice() > 0) {
-            Part part = partService.get(id);
-            part.setCode(partForm.getCode());
-            part.setName(partForm.getName());
-            part.setType(partForm.getType());
-            part.setPrice(partForm.getPrice());
-            part.setAnnotation(partForm.getAnnotation());
-            partService.update(part);
-        }
-        model.addAttribute("parts", partService.getAll());
-        return "partList";
-    }
-
-    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
-    public String deletePart(Model model, @PathVariable("id") String id) {
+    @RequestMapping("/delete/{id}")
+    public void deletePart(Model model, @PathVariable("id") String id) {
         partService.delete(id);
-        model.addAttribute("parts", partService.getAll());
-        return "partList";
     }
 }
--- src/main/java/mak/parts/partssupply/controller/SupplierController.java
+++ src/main/java/mak/parts/partssupply/controller/SupplierController.java
@@ -1,78 +1,43 @@
 package mak.parts.partssupply.controller;
 
-import mak.parts.partssupply.form.SupplierForm;
 import mak.parts.partssupply.model.Supplier;
 import mak.parts.partssupply.service.supplier.impls.SupplierServiceMongoImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
+@CrossOrigin("*")
 @RequestMapping("/api/supplier")
 public class SupplierController {
 
     @Autowired
     private SupplierServiceMongoImpl supplierService;
 
-    @RequestMapping(value = "/list", method = RequestMethod.GET)
-    public String supplierList(Model model) {
-        model.addAttribute("suppliers", supplierService.getAll());
-        return "supplierList";
+    @RequestMapping("/list")
+    public List<Supplier> getAllSuppliers() {
+        return supplierService.getAll();
     }
 
-    @RequestMapping(value = "/create", method = RequestMethod.GET)
-    public String createSupplier(Model model){
-        SupplierForm supplierForm = new SupplierForm();
-        model.addAttribute("supplierForm", supplierForm);
-        return "createSupplier";
+    @RequestMapping("/create/{code}/{name}/{address}/{phone}")
+    public Supplier createSupplier(@PathVariable("code") String code, @PathVariable("name") String name,
+                                   @PathVariable("address") String address, @PathVariable("phone") String phone){
+        return supplierService.create(new Supplier(code, name, address, phone));
     }
 
-    @RequestMapping(value = "/create", method = RequestMethod.POST)
-    public String createSupplier(Model model, @ModelAttribute("supplierForm") SupplierForm supplierForm) {
-        if (!supplierForm.getCode().isEmpty() && !supplierForm.getName().isEmpty()
-                && !supplierForm.getAddress().isEmpty() && !supplierForm.getPhone().isEmpty())
-            supplierService.create(new Supplier(supplierForm.getCode(), supplierForm.getName(),
-                                    supplierForm.getAddress(), supplierForm.getPhone()));
-        model.addAttribute("suppliers", supplierService.getAll());
-        return "supplierList";
+    @RequestMapping("/edit/{id}/{code}/{name}/{address}/{phone}")
+    public Supplier editSupplier(@PathVariable("id") String id, @PathVariable("code") String code,
+                                 @PathVariable("name") String name, @PathVariable("address") String address,
+                                 @PathVariable("phone") String phone) {
+        return supplierService.update(new Supplier(id, code, name, address, phone));
     }
 
-    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
-    public String editSupplier(Model model, @PathVariable("id") String id) {
-        Supplier supplier = supplierService.get(id);
-        SupplierForm supplierForm = new SupplierForm();
-        supplierForm.setCode(supplier.getCode());
-        supplierForm.setName(supplier.getName());
-        supplierForm.setAddress(supplier.getAddress());
-        supplierForm.setPhone(supplier.getPhone());
-        model.addAttribute("supplierForm", supplierForm);
-        return "editSupplier";
-    }
-
-    @RequestMapping(value = "/edit/{id}", method = RequestMethod.POST)
-    public String editSupplier(Model model, @PathVariable("id") String id,
-                           @ModelAttribute("supplierForm") SupplierForm supplierForm) {
-        if (!supplierForm.getCode().isEmpty() && !supplierForm.getName().isEmpty()
-                && !supplierForm.getAddress().isEmpty() && !supplierForm.getPhone().isEmpty()) {
-            Supplier supplier = supplierService.get(id);
-            supplier.setCode(supplierForm.getCode());
-            supplier.setName(supplierForm.getName());
-            supplier.setAddress(supplierForm.getAddress());
-            supplier.setPhone(supplierForm.getPhone());
-            supplierService.update(supplier);
-        }
-        model.addAttribute("suppliers", supplierService.getAll());
-        return "supplierList";
-    }
-
-    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
-    public String deleteSupplier(Model model, @PathVariable("id") String id) {
+    @RequestMapping("/delete/{id}")
+    public void deleteSupplier(@PathVariable("id") String id) {
         supplierService.delete(id);
-        model.addAttribute("suppliers", supplierService.getAll());
-        return "supplierList";
     }
 }
